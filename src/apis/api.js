0d8fccd43506c56b66beeb5f0e4209c29fedbbe9
hxia200 <hxia3@ncsu.edu>
2020-04-05 15:16:41 +0800

    https api

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://www.mocky.io/v2/5e8659a931000050008139bd',
+  baseURL: 'https://www.mocky.io/v2/5e8659a931000050008139bd',
 });
